2921ca23c0ffb98129dfadfe865f038e69f163ea
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-05-17 11:04:37 -0700

    Nearly complete navbar frontend render

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# nav-about
-Navbar and About Section
+# Project Name
+
+> Project description
+
+## Related Projects
+
+  - https://github.com/hrla29-group-targaryen/menu-cart
+  - https://github.com/hrla29-group-targaryen/reviews
+  - https://github.com/hrla29-group-targaryen/time-sponsored
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Requirements](#requirements)
+1. [Development](#development)
+
+## Usage
+
+> Some usage instructions
+
+## Requirements
+
+An `nvmrc` file is included if using [nvm](https://github.com/creationix/nvm).
+
+- Node 6.13.0
+- etc
+
+## Development
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+npm install -g webpack
+npm install
+```
+
--- client/Components/Navbar/Navbar.jsx
+++ client/Components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { GoSearch } from 'react-icons/go';
+import { GoChevronDown } from 'react-icons/go';
 import { MdClose } from 'react-icons/md';
+import { MdLocationOn } from 'react-icons/md';
+import { FaShoppingBag } from 'react-icons/fa';
 import './Navbar.css';
 
 class Navbar extends React.Component {
@@ -16,16 +19,31 @@ class Navbar extends React.Component {
         <img src="https://s3-us-west-1.amazonaws.com/kayjayhogan/GH.png"></img>
       </div>
       <div className="nav-search">
-        <div className="nav-search-address"></div>
-        <div className="nav-search-input-container">
-        <div className="test">
-          <span className="search-icon"><GoSearch /></span>          
-          <input></input>
-          <span className="close-icon"><MdClose /></span>
+        <div className="nav-search-address-container">
+          <div className="nav-search-address">
+            <span className="nav-loc-icon"><MdLocationOn /></span>
+            <span className="nav-search-address-text">6060 Center Dr</span>
+            <span className="nav-down-arrow"><GoChevronDown /></span>
+          </div>
         </div>
+        <div className="nav-search-input-container">
+          <div className="nav-search-flex">
+            <span className="nav-search-icon"><GoSearch /></span>          
+            <input placeholder="Pizza, sushi, chinese"></input>
+            <span className="nav-close-icon"><MdClose /></span>
+          </div>
         </div>
       </div>
-    </div>);
+      <div className="nav-buttons">
+        <div className="nav-sign-in">
+          <button className="nav-sign-in-btn"><strong>Sign In</strong></button>
+        </div>
+        <div className="nav-bag">
+          <span className="nav-bag-icon"><FaShoppingBag /></span>
+        </div>
+      </div>
+    </div>
+    );
   }
 }
 
