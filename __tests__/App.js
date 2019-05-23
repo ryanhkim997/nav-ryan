2d40d596ae79512e714d9d42845e4ab3c5e6c0e1
Kathleen Hogan <kathleen.j.hogan@gmail.com>
2019-05-22 20:44:47 -0700

    Implement first test

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,7 +1,23 @@
 import { shallow, mount, render } from 'enzyme';
 import React from 'react';
+import renderer from 'react-test-renderer';
 import App from '../client/Components/App';
+import Intro from '../client/Components/Intro/Intro';
+import Navbar from '../client/Components/Navbar/Navbar';
+import Modal from '../client/Components/Modals/Modal';
+import ShareModal from '../client/Components/Modals/Share/ShareModal';
+import SigninModal from '../client/Components/Modals/Signin/SigninModal';
 
-it('expect to render App component', ()=> {
-	expect(shallow(<App />).length).toEqual(1)
-})
+describe('Testing the App Component', () => {
+
+   it('renders the App component', ()=> {
+      expect(shallow(<App />).length).toEqual(1)
+   });
+
+   it('should grab a snapshot of the component', ()=> {
+		const component = renderer.create(<App />)
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+   });
+   
+});
